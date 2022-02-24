8f8e794a7b8da771b1fd2c45136c962c4d8b5801
aSeniyants <kadi4inworld@gmail.com>
2022-02-24 20:37:36 +0300

    Initial Commit

---
+++ myMap 13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myMap 13
+//
+//  Created by Аркадий on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
